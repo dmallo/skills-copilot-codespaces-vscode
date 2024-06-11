d6d3c5910cd04647c10799e1679ccf1d09b39b3d
Diego Mallo Menéndez <mallo.diego@gmail.com>
2024-06-11 09:25:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -5,3 +5,9 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const path = require('path');
+
+// get_current_step
+let get_current_step = 0;
+let get_current_step_2 = 0;
+
+
