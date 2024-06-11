d57e83646ad4e4a969528bd227ace8dc7882f08a
Diego Mallo Menéndez <mallo.diego@gmail.com>
2024-06-11 09:38:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,1 @@
-// Crate a web server
-// 1. Create a web server
-
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
-
-// get_current_step
-let get_current_step = 0;
-let get_current_step_2 = 0;
-
-
+// Create web server
